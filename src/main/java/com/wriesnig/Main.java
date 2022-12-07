bda24898668cd35b05aa33a0814d532e67a16750
Johann Wriesnig <jowriesnig@edu.aau.at>
2022-12-07 18:40:14 +0100

    changed main to use .properties variables for db credentials

--- src/main/java/com/wriesnig/Main.java
+++ src/main/java/com/wriesnig/Main.java
@@ -2,17 +2,30 @@ package com.wriesnig;
 
 import com.wriesnig.stackoverflow.db.SODatabase;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws IOException, SQLException {
         //Wie UI aussieht wird noch entschieden
         //Deshalb erstmal mit ausgewählten Usern, hier sind es die Top SO-User
         //Still todo: error handling, code cleanen, api rates, vllt auf spring verzichten
 
-        //SODatabase.initDB();
-        SODatabase.initDB("postgres", "123", "jdbc:postgresql://localhost:5432/StackOverflow");
+        Properties properties = new Properties();
+        InputStream inputStream = new FileInputStream("config.properties");
+        properties.load(inputStream);
+        inputStream.close();
+
+        String soDbUrl = properties.getProperty("dumpsDB.url");
+        String soDbUser = properties.getProperty("dumpsDB.user");
+        String soDbPassword = properties.getProperty("dumpsDB.password");
+        SODatabase.initDB(soDbUser, soDbPassword, soDbUrl);
+
         CharacterizerApplication application = new CharacterizerApplication();
         application.run();
 
