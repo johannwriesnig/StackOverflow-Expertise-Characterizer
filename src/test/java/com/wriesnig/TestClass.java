47b28f522b4753ef327950c800716b2d6bc85de9
Johann Wriesnig <jowriesnig@edu.aau.at>
2023-02-06 22:29:14 +0100

    added jacoco and pitest test reporting

---
+++ src/test/java/com/wriesnig/TestClass.java
@@ -0,0 +1,12 @@
+package com.wriesnig;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClass {
+
+    @Test
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+}
