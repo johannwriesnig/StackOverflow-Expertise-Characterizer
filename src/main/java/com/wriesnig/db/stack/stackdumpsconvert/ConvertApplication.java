61c7b401d355fdb22bca72143617c5ba41850bde
Johann Wriesnig <jowriesnig@edu.aau.at>
2023-02-17 15:10:50 +0100

    added convertapp testcases

--- src/main/java/com/wriesnig/db/stack/stackdumpsconvert/ConvertApplication.java
+++ src/main/java/com/wriesnig/db/stack/stackdumpsconvert/ConvertApplication.java
@@ -11,28 +11,22 @@ import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 
 public class ConvertApplication {
-    public static String dataPath = "db/dumpsDb/";
-
-    public ConvertApplication() {
-    }
+    public static String dataPath = "db/dumpsDb";
 
     public void run() {
         System.setProperty("jdk.xml.totalEntitySizeLimit", String.valueOf(Integer.MAX_VALUE));
         SAXParserFactory factory = SAXParserFactory.newInstance();
 
         try {
-            ConvertJob usersConvertJob = new ConvertJob(new UsersInfo(), dataPath + "xml/Users.xml", factory.newSAXParser());
-            ConvertJob postsConvertJob = new ConvertJob(new PostsInfo(), dataPath + "xml/Posts.xml", factory.newSAXParser());
-            ConvertJob votesConvertJob = new ConvertJob(new VotesInfo(), dataPath + "xml/Votes.xml", factory.newSAXParser());
+            ConvertJob usersConvertJob = new ConvertJob(new UsersInfo(), dataPath + "/" + "xml/Users.xml", factory.newSAXParser());
+            ConvertJob postsConvertJob = new ConvertJob(new PostsInfo(), dataPath + "/" + "xml/Posts.xml", factory.newSAXParser());
+            ConvertJob votesConvertJob = new ConvertJob(new VotesInfo(), dataPath + "/" + "xml/Votes.xml", factory.newSAXParser());
 
             usersConvertJob.convert();
             votesConvertJob.convert();
             postsConvertJob.convert();
-        } catch (SAXException | ParserConfigurationException e) {
-            Logger.error("Error while converting xml", e);
-        } catch (IOException e) {
-            Logger.error("IO Error while converting xml", e);
-            throw new RuntimeException();
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            Logger.error("Converting xml to csv failed.", e);
         }
     }
 }
--- src/test/java/com/wriesnig/db/stack/stackdumpsconvert/ConvertApplicationTest.java
+++ src/test/java/com/wriesnig/db/stack/stackdumpsconvert/ConvertApplicationTest.java
@@ -1,5 +1,59 @@
 package com.wriesnig.db.stack.stackdumpsconvert;
 
-public class ConvertApplicationTest {
+import com.wriesnig.utils.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+import java.io.IOException;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ConvertApplicationTest {
+    private ConvertApplication convertApplication;
+
+    @BeforeEach
+    public void setUp(){
+        ConvertApplication.dataPath = "src/main/resources/test/convertApp";
+        convertApplication = new ConvertApplication();
+    }
+
+    @Test
+    public void runConvertJobs() throws IOException {
+        try(MockedConstruction<ConvertJob> convertJob = mockConstruction(ConvertJob.class,
+                (mock,context)->{
+
+        })){
+            convertApplication.run();
+            assertEquals(3, convertJob.constructed().size());
+            for(ConvertJob job: convertJob.constructed())
+                verify(job, times(1)).convert();
+        }
+    }
+
+    @Test
+    public void convertJobThrowsException() throws IOException {
+        try(MockedStatic<Logger> mockedLogger = mockStatic(Logger.class);
+            MockedConstruction<ConvertJob> convertJob = mockConstruction(ConvertJob.class,
+                (mock,context)->{
+                    doThrow(IOException.class).when(mock).convert();
+                })){
+            convertApplication.run();
+
+            List<ConvertJob> constructedJobs = convertJob.constructed();
+            assertEquals(3, constructedJobs.size());
+            verify(constructedJobs.get(0), times(1)).convert();
+            verify(constructedJobs.get(1), times(0)).convert();
+            verify(constructedJobs.get(2), times(0)).convert();
+            mockedLogger.verify(()-> Logger.error(any(), any()),times(1));
+        }
+    }
+
+    @AfterEach
+    public void tearDown(){
+        convertApplication = null;
+    }
 }
