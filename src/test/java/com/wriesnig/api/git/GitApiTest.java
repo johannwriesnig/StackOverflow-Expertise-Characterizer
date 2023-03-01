5e0811d33f7ec5e5a195094c7ba3b370adda28fb
Johann Wriesnig <jowriesnig@edu.aau.at>
2023-03-01 11:03:59 +0100

    added gitapi testcases

--- src/test/java/com/wriesnig/api/git/GitApiTest.java
+++ src/test/java/com/wriesnig/api/git/GitApiTest.java
@@ -3,72 +3,70 @@ package com.wriesnig.api.git;
 import com.wriesnig.utils.Logger;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
-import org.mockito.Answers;
 import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
+
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 public class GitApiTest {
 
-    /*
-    @Test
-    public void emptyStreamToString() throws IOException {
-        String expected = "";
-
-        assertEquals(expected, GitApi.getStringFromStream(InputStream.nullInputStream()));
-    }
-
     @Test
     public void retrieveUserByLogin() throws IOException {
         String fileName = "src/main/resources/test/apiResponses/git/loginResponse1.txt";
-        try (MockedStatic<GitApi> mocked = mockStatic(GitApi.class)) {
-            mocked.when(() -> GitApi.getStreamFromAPICall(anyString())).thenReturn(new FileInputStream(fileName));
-            mocked.when(() -> GitApi.getUserByLogin(any())).thenCallRealMethod();
-            mocked.when(() -> GitApi.getStringFromStream(any())).thenCallRealMethod();
+        try (MockedStatic<GitApi> mockedGitApi = mockStatic(GitApi.class)) {
+
+            mockedGitApi.when(() -> GitApi.getStreamFromAPICall(anyString())).thenReturn(new FileInputStream(fileName));
+            mockedGitApi.when(() -> GitApi.getUserByLogin(any())).thenCallRealMethod();
+            mockedGitApi.when(() -> GitApi.getStringFromStream(any())).thenCallRealMethod();
             GitUser user = GitApi.getUserByLogin("octocat");
             assertNotNull(user);
             assertEquals("octocat", user.getLogin());
+            mockedGitApi.verify(()->GitApi.getStreamFromAPICall(anyString()), times(1));
+            mockedGitApi.verify(()->GitApi.getStringFromStream(any()), times(1));
         }
     }
 
     @Test
     public void retrieveUsersByFullName() throws IOException {
         String fileName = "src/main/resources/test/apiResponses/git/fullNameResponse.txt";
-        try (MockedStatic<GitApi> mocked = mockStatic(GitApi.class)) {
-            mocked.when(() -> GitApi.getStreamFromAPICall(anyString())).thenReturn(new FileInputStream(fileName));
-            mocked.when(() -> GitApi.getUsersByFullName(any())).thenCallRealMethod();
-            mocked.when(() -> GitApi.getStringFromStream(any())).thenCallRealMethod();
+        try (MockedStatic<GitApi> mockedGitApi = mockStatic(GitApi.class)) {
+            mockedGitApi.when(() -> GitApi.getStreamFromAPICall(anyString())).thenReturn(new FileInputStream(fileName));
+            mockedGitApi.when(() -> GitApi.getUsersByFullName(any())).thenCallRealMethod();
+            mockedGitApi.when(() -> GitApi.getStringFromStream(any())).thenCallRealMethod();
             ArrayList<String> logins = GitApi.getUsersByFullName("Max Mustermann");
             assertEquals(2, logins.size());
+            mockedGitApi.verify(()->GitApi.getStreamFromAPICall(anyString()), times(1));
+            mockedGitApi.verify(()->GitApi.getStringFromStream(any()), times(1));
         }
     }
 
     @Test
     public void retrieveRepos() throws IOException {
         String fileName = "src/main/resources/test/apiResponses/git/reposResponse.txt";
-        try (MockedStatic<GitApi> mocked = mockStatic(GitApi.class)) {
-            mocked.when(() -> GitApi.getStreamFromAPICall(anyString())).thenReturn(new FileInputStream(fileName));
-            mocked.when(() -> GitApi.getReposByLogin(any())).thenCallRealMethod();
-            mocked.when(() -> GitApi.getStringFromStream(any())).thenCallRealMethod();
+        try (MockedStatic<GitApi> mockedGitApi = mockStatic(GitApi.class)) {
+            mockedGitApi.when(() -> GitApi.getStreamFromAPICall(anyString())).thenReturn(new FileInputStream(fileName));
+            mockedGitApi.when(() -> GitApi.getReposByLogin(any())).thenCallRealMethod();
+            mockedGitApi.when(() -> GitApi.getStringFromStream(any())).thenCallRealMethod();
             ArrayList<Repo> repos = GitApi.getReposByLogin("johannwriesnig");
-            assertEquals(2, repos.size());
+            assertEquals(3, repos.size());
             assertEquals("johannwriesnig/Runner", repos.get(0).getName());
+            assertEquals("johannwriesnig/SE2_Einzelbeispiel", repos.get(1).getName());
+            assertEquals("johannwriesnig/ControlNet", repos.get(2).getName());
+            mockedGitApi.verify(()->GitApi.getStreamFromAPICall(anyString()), times(1));
+            mockedGitApi.verify(()->GitApi.getStringFromStream(any()), times(1));
         }
     }
 
     @Test
-    public void getStringFromInputStreamThrowsIOException() {
+    public void getStringFromInputStreamThrowsIOException() throws IOException {
         try (MockedStatic<Logger> mockedLogger = mockStatic(Logger.class);
              MockedConstruction<BufferedReader> mockedBufferedReader = mockConstruction(BufferedReader.class,
                      (mock, context) -> {
@@ -77,20 +75,13 @@ public class GitApiTest {
             InputStream inputStream = InputStream.nullInputStream();
             GitApi.getStringFromStream(inputStream);
             mockedLogger.verify(() -> Logger.error(any(), any()), times(1));
+            assertEquals(1, mockedBufferedReader.constructed().size());
+            BufferedReader bufferedReader = mockedBufferedReader.constructed().get(0);
+            verify(bufferedReader, times(1)).read();
         }
     }
+    
 
-    @Test
-    public void getStreamFromApiCallThrowsIOException() {
-        try (MockedStatic<Logger> mockedLogger = mockStatic(Logger.class);
-             MockedStatic<GitApi> mockedGitApi = mockStatic(GitApi.class)){
-            mockedGitApi.when(() -> GitApi.getInputStream(any())).thenThrow(IOException.class);
-            mockedGitApi.when(() -> GitApi.getStreamFromAPICall(any())).thenCallRealMethod();
-            String path = "path";
-            GitApi.getStreamFromAPICall(path);
-            mockedLogger.verify(() -> Logger.error(any(), any()), times(1));
-        }
-    }
 
     @Test
     public void downloadingReposThrowsIOException() {
@@ -142,5 +133,11 @@ public class GitApiTest {
             assertTrue(repoDir.isDirectory());
             FileUtils.deleteDirectory(repoDir);
         }
-    }*/
+    }
+
+    @Test
+    public void emptyStreamToString() throws IOException {
+        String expected = "";
+        assertEquals(expected, GitApi.getStringFromStream(InputStream.nullInputStream()));
+    }
 }
