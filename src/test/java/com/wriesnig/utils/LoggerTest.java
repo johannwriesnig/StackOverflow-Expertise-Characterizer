4213a6802df0de93d60f8a166fc652a0c4fd644f
Johann Wriesnig <jowriesnig@edu.aau.at>
2023-03-08 18:51:16 +0100

    added tests to LoggerTest

--- src/test/java/com/wriesnig/utils/LoggerTest.java
+++ src/test/java/com/wriesnig/utils/LoggerTest.java
@@ -10,57 +10,63 @@ import static org.mockito.Mockito.*;
 
 public class LoggerTest {
     private ByteArrayOutputStream systemOut;
+    private ByteArrayOutputStream systemErrOut;
     private static final String toLog = "This is a test sentence.";
 
     @BeforeEach
     public void setUp(){
        systemOut = new ByteArrayOutputStream();
+       systemErrOut = new ByteArrayOutputStream();
        System.setOut(new PrintStream(systemOut));
+       System.setErr(new PrintStream(systemErrOut));
        Logger.activatePrinting();
     }
 
     @Test
-    public void logInfo(){
+    public void shouldPrintInfo(){
         Logger.info(toLog);
         assertTrue(systemOut.toString().contains(toLog));
     }
 
     @Test
-    public void deactivatePrintingAndLogInfo(){
+    public void shouldNotPrintInfo(){
         Logger.deactivatePrinting();
         Logger.info(toLog);
         assertFalse(systemOut.toString().contains(toLog));
     }
 
     @Test
-    public void logError(){
+    public void shouldPrintError(){
         Logger.error(toLog);
         assertTrue(systemOut.toString().contains(toLog));
     }
 
     @Test
-    public void deactivatePrintingAndLogError(){
+    public void shouldNotPrintError(){
         Logger.deactivatePrinting();
         Logger.error(toLog);
         assertFalse(systemOut.toString().contains(toLog));
     }
 
     @Test
-    public void logErrorWithException(){
+    public void shouldPrintErrorAndExceptionStackTrace(){
         Exception e = spy(new Exception());
         Logger.error(toLog, e);
         assertTrue(systemOut.toString().contains(toLog));
+        assertFalse(systemErrOut.toString().isEmpty());
         verify(e).printStackTrace();
     }
 
     @Test
-    public void deactivatePrintingAndLogErrorWithException(){
+    public void shouldNotPrintErrorOrException(){
         Logger.deactivatePrinting();
         Logger.error(toLog, new Exception());
         assertFalse(systemOut.toString().contains(toLog));
+        assertTrue(systemErrOut.toString().isEmpty());
     }
+
     @Test
-    public void loggingAfterUsingActivateAndDeactivatePrinting(){
+    public void testLoggingAfterActivateAndDeactivatePrinting(){
         Logger.deactivatePrinting();
         Logger.activatePrinting();
         Logger.info(toLog);
@@ -75,6 +81,7 @@ public class LoggerTest {
     @BeforeEach
     public void tearDown(){
         systemOut = null;
+        systemErrOut = null;
     }
 
 }
